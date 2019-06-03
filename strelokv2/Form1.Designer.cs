9bb76052cff0f49be766f4f71fe9239ff320e344
NoD1KuN <51018412+NoD1KuN@users.noreply.github.com>
2019-06-03 16:37:57 +0300

    Добавьте файлы проекта.

---
+++ strelokv2/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace strelokv2
+{
+    partial class Strelok
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.уровеньToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.учебнаяСтрельбаxyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.боеваяСтрельбаxyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.боеваяСтрельбаuRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.уровеньToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(932, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // уровеньToolStripMenuItem
+            // 
+            this.уровеньToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.учебнаяСтрельбаxyToolStripMenuItem,
+            this.боеваяСтрельбаxyToolStripMenuItem,
+            this.боеваяСтрельбаuRToolStripMenuItem});
+            this.уровеньToolStripMenuItem.Name = "уровеньToolStripMenuItem";
+            this.уровеньToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.уровеньToolStripMenuItem.Text = "Уровень";
+            // 
+            // учебнаяСтрельбаxyToolStripMenuItem
+            // 
+            this.учебнаяСтрельбаxyToolStripMenuItem.Name = "учебнаяСтрельбаxyToolStripMenuItem";
+            this.учебнаяСтрельбаxyToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.учебнаяСтрельбаxyToolStripMenuItem.Text = "Учебная стрельба (x,y)";
+            // 
+            // боеваяСтрельбаxyToolStripMenuItem
+            // 
+            this.боеваяСтрельбаxyToolStripMenuItem.Name = "боеваяСтрельбаxyToolStripMenuItem";
+            this.боеваяСтрельбаxyToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.боеваяСтрельбаxyToolStripMenuItem.Text = "Боевая стрельба (x,y)";
+            // 
+            // боеваяСтрельбаuRToolStripMenuItem
+            // 
+            this.боеваяСтрельбаuRToolStripMenuItem.Name = "боеваяСтрельбаuRToolStripMenuItem";
+            this.боеваяСтрельбаuRToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.боеваяСтрельбаuRToolStripMenuItem.Text = "Боевая стрельба (u,R)";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Учебная стрельба (x,y)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 521);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Укажите координаты центра относительно глаза:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 542);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x =";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 542);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(127, 542);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "y =";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 541);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(80, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Выстрел";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(665, 521);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "try";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(668, 541);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "hit";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 574);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(171, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "размер области 600*360";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(295, 574);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(223, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "за единицу измерения берем 20";
+            // 
+            // Strelok
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 603);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Strelok";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Стрелок";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem уровеньToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem учебнаяСтрельбаxyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem боеваяСтрельбаxyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem боеваяСтрельбаuRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ strelokv2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace strelokv2
+{
+    public partial class Strelok : Form
+    {
+        public Random r = new Random();
+        public static Graphics g;
+        public static int a, b, with, heght;
+        public static int trai = 0;
+        public static int hitcount = 0;
+        public Strelok()
+        {
+
+            InitializeComponent();
+        }
+
+        public void trycount()
+        {
+            label5.Text = Convert.ToString("Попытка №" + " " + (trai + 1));
+            label6.Text = Convert.ToString("Попаданий" + " " + hitcount);
+        }
+
+        public void point()
+        {
+            a = r.Next(20, 350);
+            b = r.Next(20, 350);
+            int with = 5, heght = 5;
+            SolidBrush brush = new SolidBrush(Color.Black);
+            g.DrawEllipse(Pens.Black, a, b, with, heght);
+            g.FillEllipse(brush, a, b, with, heght);
+        }
+
+        public void field()
+        {
+            
+            g.DrawLine(new Pen(Color.Red, 5), new Point(0, 0), new Point(2000, 0));
+
+            g.DrawLine(new Pen(Color.Red, 5), new Point(0, 0), new Point(0, 2000));
+            int x1 = 20, x2 = 20, y1 = 0, y2 = 10;
+            for (int i = 0; i < 50; i++)
+            {
+                g.DrawLine(new Pen(Color.Red, 2), new Point(x1, y1), new Point(x2, y2));
+                x1 += 20; x2 += 20;
+            }
+            int x3 = 0, y3 = 20, x4 = 10, y4 = 20;
+            for (int i = 0; i < 50; i++)
+            {
+                g.DrawLine(new Pen(Color.Red, 2), new Point(x3, y3), new Point(x4, y4));
+                y3 += 20; y4 += 20;
+            }
+        }
+        public void target()
+        {
+            int x = 350;
+            int y = 150;
+            int width = 200; int height = 200;
+            for (int i = 0; i < 10; i++)
+            {
+                SolidBrush br;
+                if (i % 2 == 0)
+                    br = new SolidBrush(Color.Black);
+                else
+                    br = new SolidBrush(Color.White);
+                g.DrawEllipse(Pens.Black, x, y, width, height);
+                g.FillEllipse(br, x, y, width, height);
+                height -= 24; width -= 24;
+                x += 12; y += 12;
+            }          
+        }
+
+        public void shoot()
+        {
+            int x = Convert.ToInt32(textBox1.Text) + a;
+            int y = Convert.ToInt32(textBox3.Text) + b;
+
+            SolidBrush brush = new SolidBrush(Color.Red);
+            g.DrawEllipse(Pens.Black, x, y, with, heght);
+            g.FillEllipse(brush, x, y, 5, 5);
+
+            if (Math.Pow(x - 450, 2) + Math.Pow(y - 250, 2) <= 10000)
+                MessageBox.Show("Попал");
+            else
+            
+                MessageBox.Show("Не попал");
+            if (Math.Pow(x - 450, 2) + Math.Pow(y - 250, 2) <= 10000)
+            { hitcount++; }
+                
+                trai++;
+            trycount();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trycount();
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = panel1.CreateGraphics();
+
+            point();
+            field();
+            target();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shoot();
+        }
+    }
+}
